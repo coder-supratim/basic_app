3ace760be4f0a9f709e0eee3c1e583ec2c1e4672
coder-supratim <supratim.arya@gmail.com>
2025-12-16 22:37:54 -0500

    added basic genrator code example

--- src/main.py
+++ src/main.py
@@ -10,6 +10,23 @@ def main():
     #comprehension example of set
     unique_providers = {name for name in ai_provider}
     print("Unique AI Providers:", unique_providers)
+    dictioanary_comprehension_example()
+    generate_comprehension_example()
+
+def dictioanary_comprehension_example():
+    ai_provider = {"openai": 800, "azure": 230, "anthropic": 588, "cohere": 67, "ai21": 570}
+    # comprehension example of dictionary 
+    #high value providers for last 30 days
+    high_value_providers = {name: value  / 30 for name, value in ai_provider.items() if value > 300}
+    print("High Value AI Providers:", high_value_providers)
+
+def generate_comprehension_example():
+    # Function to calculate total number of providers with value greater than 300
+    ai_provider = {"openai": 800, "azure": 230, "anthropic": 588, "cohere": 67, "ai21": 570}
+    total_provides = sum(value for value, value in ai_provider.items() if value > 300)
+    print("Total High Value Providers:", total_provides)
+    pass
+
 
 if __name__ == "__main__":
     main()
