fb24c066d1d3b719b275f8eca3bba4e637b9a754
coder-supratim <supratim.arya@gmail.com>
2026-01-25 18:26:44 -0500

    sample coding question #1

---
+++ src/coding_tests/first_non_repeating_char.py
@@ -0,0 +1,34 @@
+# Given a string s of lowercase and uppercase English letters, the task is to find the first non-repeating character. If there is no such character, return '$'.
+
+# Examples: 
+
+# Input: s = "geeksForgeeks"
+# Output: 'f'
+# Explanation: 'f' is the first character in the string which does not repeat.
+
+# Input: s = "Racecar"
+# Output: 'e'
+# Explanation: 'e' is the only character in the string which does not repeat.
+
+# Input: "aabbccc"
+# Output: '$'
+# Explanation: All the characters in the given string are repeating.
+
+
+def find_first_non_repeating_char(s: str) -> str:
+	char_count = {}
+	s = s.casefold()  # Normalize case to handle both lowercase and uppercase
+	for char in s:
+		if char in char_count:
+			char_count[char] += 1
+		else:
+			char_count[char] = 1
+
+	for char in s:
+		if char_count[char] == 1:
+			return char
+	return '$'
+
+
+s = find_first_non_repeating_char("Racecar")
+print(s)
