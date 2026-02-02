17438eda68de1b458759c10ed4c48181eb89903b
coder-supratim <supratim.arya@gmail.com>
2026-02-01 21:09:34 -0500

    count word frequency

---
+++ src/coding_tests/count_word_frequency.py
@@ -0,0 +1,22 @@
+def findWordFrequency (text) :
+	words = text.lower().split()
+	freq = {}
+	
+	for word in words:
+		if word in freq:
+			freq[word] += 1
+		else:
+			freq[word] = 1
+	return freq
+
+
+# Shorter version using dict.get() method
+def shortWordFrequency(text):
+	words = text.lower().split()
+	freq = {}
+
+	for word in words:
+		freq[word] = freq.get(word,0) +1 
+	return freq
+print(shortWordFrequency("The quick brown fox jumps over the lazy dog"))
+print(findWordFrequency("The quick brown fox jumps over the lazy dog"))
